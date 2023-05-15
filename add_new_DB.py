9c04fe9645f309916a492b524ef3f50f654321c0
Gabriel <gabrielperren@gmail.com>
2023-05-15 16:11:21 -0300

    large code clean up

--- add_new_DB.py
+++ add_new_DB.py
@@ -3,7 +3,6 @@ import datetime
 import json
 import csv
 import pandas as pd
-# from astropy.coordinates import angular_separation
 from modules import fastMP_process, DBs_combine
 
 
@@ -93,83 +92,6 @@ def main(
     # fastMP_process.run(new_DB, UCC_cat)
     fastMP_process.run(
         fastMP, new_DB, frames_path, frames_ranges, UCC_cat, GCs_cat, out_path)
-
-
-# def dups_identify(df, N_dups):
-#     """
-#     Find the closest clusters to all clusters
-#     """
-#     x, y = df['GLON'], df['GLAT']
-#     pmRA, pmDE, plx = df['pmRA'], df['pmDE'], df['plx']
-#     coords = np.array([x, y]).T
-#     # Find the distances to all clusters, for all clusters
-#     dist = cdist(coords, coords)
-#     # Change distance to itself from 0 to inf
-#     msk = dist == 0.
-#     dist[msk] = np.inf
-
-#     dups_fnames = []
-#     for i, cl in enumerate(dist):
-#         idx = np.argsort(cl)[:N_dups]
-
-#         dups_fname = []
-#         for j in idx:
-#             # Angular distance in arcmin (rounded)
-#             d = round(angular_separation(x[i], y[i], x[j], y[j]) * 60, 2)
-#             # PMs distance
-#             pm_d = np.sqrt((pmRA[i]-pmRA[j])**2 + (pmDE[i]-pmDE[j])**2)
-#             # Parallax distance
-#             plx_d = abs(plx[i] - plx[j])
-
-#             dup_flag = duplicate_find(d, pm_d, plx_d, plx[i])
-
-#             if dup_flag:
-#                 fname = df['fnames'][j].split(';')[0]
-#                 dups_fname.append(fname)
-
-#         if dups_fname:
-#             # print(i, df['fnames'][i], len(dups_fname), dups_fname)
-#             dups_fname = ";".join(dups_fname)
-#         else:
-#             dups_fname = 'nan'
-
-#         dups_fnames.append(dups_fname)
-
-#     return dups_fnames
-
-
-# def duplicate_find(d, pm_d, plx_d, plx):
-#     """
-#     Identify a cluster as a duplicate following an arbitrary definition
-#     that depends on the parallax
-#     """
-#     if plx >= 4:
-#         rad, plx_r, pm_r = 15, 0.5, 1
-#     elif 3 <= plx and plx < 4:
-#         rad, plx_r, pm_r = 10, 0.25, 0.5
-#     elif 2 <= plx and plx < 3:
-#         rad, plx_r, pm_r = 5, 0.15, 0.25
-#     elif 1 <= plx and plx < 2:
-#         rad, plx_r, pm_r = 2.5, 0.1, 0.15
-#     else:
-#         rad, plx_r, pm_r = 1, 0.05, 0.1
-
-#     if not np.isnan(plx_d) and not np.isnan(pm_d):
-#         if pm_d < pm_r and plx_d < plx_r and d < rad:
-#             return True
-#     elif not np.isnan(plx_d) and np.isnan(pm_d):
-#         if plx_d < plx_r and d < rad:
-#             return True
-#     elif np.isnan(plx_d):
-#         rad, pm_r = 5, 0.5
-#         if not np.isnan(pm_d):
-#             if pm_d < pm_r and d < rad:
-#                 return True
-#         else:
-#             if d < rad:
-#                 return True
-
-#     return False
 
 
 if __name__ == '__main__':
