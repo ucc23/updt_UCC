295e094b579e26bd481d160d1cc0ed637a4f7e27
Gabriel <gabrielperren@gmail.com>
2023-06-28 11:13:15 -0300

    working on this

--- modules/ucc_plots.py
+++ modules/ucc_plots.py
@@ -13,7 +13,7 @@ plt.style.use('science')
 # matplotlib.rcParams.update({'font.size': 22})
 
 
-def make_plot(out_path, fnames, df, N_membs_min, cmap='cividis', dpi=200):
+def make_plot(out_path, fname0, df, N_membs_min, cmap='viridis', dpi=200):
     """
     """
     # Select members and field stars
@@ -56,6 +56,12 @@ def make_plot(out_path, fnames, df, N_membs_min, cmap='cividis', dpi=200):
 
     ax2.set_xlabel("pmRA [mas/yr]")
     ax2.set_ylabel("pmDE [mas/yr]")
+    # Plot limits
+    pmra_c, pmde_c = np.median(df_membs['pmRA']), np.median(df_membs['pmDE'])
+    xrad = np.percentile(abs(pmra_c-df_membs['pmRA']), 95) * 2
+    yrad = np.percentile(abs(pmde_c-df_membs['pmDE']), 95) * 2
+    ax2.set_xlim(pmra_c-xrad, pmra_c+xrad)
+    ax2.set_ylim(pmde_c-yrad, pmde_c+yrad)
 
     ax3.scatter(df_field['Plx'], df_field['Gmag'], c='grey', alpha=.3, s=10,
                 marker='x')
@@ -65,6 +71,11 @@ def make_plot(out_path, fnames, df, N_membs_min, cmap='cividis', dpi=200):
     ax3.invert_yaxis()
     ax3.set_xlabel("Plx [mas]")
     ax3.set_ylabel("Gmag")
+    # Plot limits
+    plx_c = np.median(df_membs['Plx'])
+    xrad = np.percentile(abs(plx_c-df_membs['Plx']), 95) * 2
+    ax3.set_xlim(plx_c-xrad, plx_c+xrad)
+    ax3.set_ylim(max(df_membs['Gmag']) + .2, min(df_membs['Gmag']) - .5)
 
     ax4.scatter(
         df_field['BP-RP'], df_field['Gmag'], c='grey', alpha=.3, ec='w', lw=.35,
@@ -75,10 +86,14 @@ def make_plot(out_path, fnames, df, N_membs_min, cmap='cividis', dpi=200):
     ax4.invert_yaxis()
     ax4.set_xlabel("BP-RP")
     ax4.set_ylabel("Gmag")
+    # Plot limits
+    x_max_cmd, x_min_cmd, y_min_cmd, y_max_cmd = diag_limits(
+        df_membs['BP-RP'], df_membs['Gmag'])
+    ax4.set_xlim(x_min_cmd, x_max_cmd)
+    ax4.set_ylim(y_min_cmd, y_max_cmd)
 
     fig.tight_layout()
-    clname = fnames.split(';')[0]
-    plt.savefig(out_path + clname + ".png", dpi=dpi)
+    plt.savefig(out_path + fname0 + ".png", dpi=dpi)
 
 
 def colorbar(mappable):
@@ -112,3 +127,21 @@ def plx_size(plx, max_s=100):
     h = -m * plx.min()
     s = 10 + (h + m * plx)
     return s
+
+
+def diag_limits(phot_x, phot_y):
+    """
+    Define plot limits for *all* photometric diagrams.
+    """
+    x_delta = np.nanmax(phot_x) - np.nanmin(phot_x)
+    x_min_cmd = min(phot_x) - .2 * x_delta
+    x_max_cmd = max(phot_x) + .1 * x_delta
+
+    # y_median, y_std = np.nanmedian(phot_y), np.nanstd(phot_y)
+    # y limits.
+    y_min_cmd = np.nanmax(phot_y) + .5
+    # If photometric axis y is a magnitude, make sure the brightest star
+    # is always plotted.
+    y_max_cmd = np.nanmin(phot_y) - 1.
+
+    return x_max_cmd, x_min_cmd, y_min_cmd, y_max_cmd
