d636d77f3f0ef7ad89be8ddddac4e64af2d1e1f3
Gabriel <gabrielperren@gmail.com>
2024-02-07 17:24:49 -0300

    move fixed params to HARDCODED

---
+++ scripts/HARDCODED.py
@@ -0,0 +1,28 @@
+# Folder where the databases and required files are located
+dbs_folder = "../databases/"
+# Ouput folder where the new UCC version is stored
+UCC_folder = "../zenodo/"
+# Database of listed GCs
+GCs_cat = "globulars.csv"
+
+# Name of JSON file with DBs data
+all_DBs_json = "all_dbs.json"
+
+# File where OCs members data will be stored
+new_OCs_fpath = "../new_OCs_data.csv"
+
+# Root path to the UCC folder with all the repos
+root_UCC_path = "../../"
+# Folder where the pages are stored
+pages_folder = "ucc/_pages"
+# Path to the OCs JSON file
+clusters_json = "ucc/clusters.json"
+
+# Folder where the md entries are stored
+md_folder = "ucc/_clusters"
+# Folder where the notebooks are stored
+ntbks_folder = "notebooks"
+# Folder where the member files are stored
+members_folder = "datafiles"
+# Folder where the plots are stored
+plots_folder = "plots"
