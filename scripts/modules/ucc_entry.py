80f9e496ae901400b4794222269ecfc7197298c2
Gabriel <gabrielperren@gmail.com>
2024-12-04 10:39:32 -0300

    add images carousel, still working on notebooks

--- scripts/modules/ucc_entry.py
+++ scripts/modules/ucc_entry.py
@@ -1,4 +1,13 @@
 from datetime import datetime
+from pathlib import Path
+
+import numpy as np
+from HARDCODED import (
+    plots_folder,
+    root_UCC_path,
+)
+
+from modules import combine_UCC_new_DB
 
 header = """---
 layout: post
@@ -78,7 +87,7 @@ no_membs_50_warning = """
 """
 
 nasa_simbad_url = """
-> <p style="text-align:center; font-weight: bold; font-size:20px">Search object in <a href="https://ui.adsabs.harvard.edu/search/q=%20collection%3Aastronomy%20body%3A%22XXNASAXX%22&sort=date%20desc%2C%20bibcode%20desc&p_=0" target="_blank">NASA/SAO ADS</a> | <a href="https://simbad.cds.unistra.fr/simbad/sim-id-refs?Ident=XXSIMBADXX" target="_blank">Simbad</a></p>
+> <p style="text-align:center; font-weight: bold; font-size:20px">Search object in <a data-umami-event="nasa_search" href="https://ui.adsabs.harvard.edu/search/q=%20collection%3Aastronomy%20body%3A%22XXNASAXX%22&sort=date%20desc%2C%20bibcode%20desc&p_=0" target="_blank">NASA/SAO ADS</a> | <a data-umami-event="simbad_search" href="https://simbad.cds.unistra.fr/simbad/sim-id-refs?Ident=XXSIMBADXX" target="_blank">Simbad</a></p>
 """
 
 #  Mobile url
@@ -87,23 +96,23 @@ nasa_simbad_url = """
 posit_table_top = """\n
 ### Position in UCC and published works (not exhaustive)
 
-| Reference    | RA    | DEC   | plx  | pmRA  | pmDE   |  Rv  |
+| Reference    | RA    | DEC   | Plx  | pmRA  | pmDE   |  Rv  |
 | :---         | :---: | :---: | :---: | :---: | :---: | :---: |
 """
 
 cds_simbad_url = """
-> <p style="text-align:center; font-weight: bold; font-size:20px">Search coordinates in <a href="https://cdsportal.u-strasbg.fr/?target=RADEC_CDS" target="_blank">CDS</a> | <a href="https://simbad.cds.unistra.fr/mobile/object_list.html?coord=RADEC_SMB&output=json&radius=5&userEntry=XCLUSTX" target="_blank">Simbad</a></p>
+> <p style="text-align:center; font-weight: bold; font-size:20px">Search coordinates in <a data-umami-event="cds_coord_search" href="https://cdsportal.u-strasbg.fr/?target=RADEC_CDS" target="_blank">CDS</a> | <a data-umami-event="simbad_coord_search" href="https://simbad.cds.unistra.fr/mobile/object_list.html?coord=RADEC_SMB&output=json&radius=5&userEntry=XCLUSTX" target="_blank">Simbad</a></p>
 """
 
 cl_plot = """
 ### Plots for selected probable members
 
-![CLUSTER](https://raw.githubusercontent.com/ucc23/{}/main/plots/{}.webp)
+{}
 
 """
 
 notebook_url = """
-> <p style="text-align:center; font-weight: bold; font-size:20px">Explore data in <a href="https://colab.research.google.com/github/UCC23/{}/blob/master/notebooks/{}.ipynb" target="_blank">Colab</a></p>
+> <p style="text-align:center; font-weight: bold; font-size:20px">Explore data in <a data-umami-event="colab" href="https://colab.research.google.com/github/UCC23/{}/blob/master/notebooks/{}.ipynb" target="_blank">Colab</a></p>
 """
 
 fpars_table_top = """\n
@@ -116,7 +125,7 @@ fpars_table_top = """\n
 close_table_top = """\n
 ### Probable <a href="https://ucc.ar/faq#probable-duplicates" title="See FAQ for definition of proximity">duplicates</a> and clusters in proximity
 
-| Cluster | P (%) | RA    | DEC   | plx   | pmRA  | pmDE  | Rv    |
+| Cluster | P (%) | RA    | DEC   | Plx   | pmRA  | pmDE  | Rv    |
 | :---:   | :---: | :---: | :---: | :---: | :---: | :---: | :---: |
 """
 
@@ -126,51 +135,53 @@ data_foot = """\n
 """
 
 
-def main(
-    cl_names,
-    Qfold,
-    fname,
-    ucc_id,
-    C1,
-    C2,
-    abcd_c,
-    r_50,
-    N_50,
-    ra,
-    dec,
-    fpars_table,
-    posit_table,
-    close_table,
-):
+def main(df_UCC, UCC_cl, DBs_json, DBs_full_data, fname0, Qfold):
     """ """
+    # DBs where this cluster is present
+    DBs = UCC_cl["DB"].split(";")
+
+    # Indexes where this cluster is present in each DB
+    DBs_i = UCC_cl["DB_i"].split(";")
+
+    cl_names = UCC_cl["ID"].split(";")
+
+    posit_table = positions_in_lit(DBs_json, DBs_full_data, DBs, DBs_i, UCC_cl)
+    img_cont = carousel_div(cl_names[0], Qfold, fname0)
+    close_table = close_cat_cluster(df_UCC, UCC_cl)
+    fpars_table = fpars_in_lit(DBs_json, DBs_full_data, DBs, DBs_i)
+
+    # Color used by the 'C1' classification
+    abcd_c = UCC_color(UCC_cl["C3"])
+
+    # Start generating the md file
     txt = ""
     txt += header.format(cl_names[0])
     if len(cl_names) > 1:
         txt += more_names.replace("cl_names_str", "; ".join(cl_names[1:]))
 
-    rad_deg = round(2 * (r_50 / 60.0), 3)
+    rad_deg = round(2 * (UCC_cl["r_50"] / 60.0), 3)
     txt += (
         aladin_snippet.replace("QFOLD", str(Qfold))
-        .replace("FNAME", str(fname))
+        .replace("FNAME", str(fname0))
         .replace("RAD_DEG", str(rad_deg))
-        .replace("RA_ICRS", str(ra))
-        .replace("DE_ICRS", str(dec))
+        .replace("RA_ICRS", str(UCC_cl["RA_ICRS_m"]))
+        .replace("DE_ICRS", str(UCC_cl["DE_ICRS_m"]))
     )
 
     txt += (
-        table_right_col.replace("UCC_ID", ucc_id)
-        .replace("Class1", str(C1))
-        .replace("Class2", str(C2))
+        table_right_col.replace("UCC_ID", UCC_cl["UCC_ID"])
+        .replace("Class1", str(UCC_cl["C1"]))
+        .replace("Class2", str(UCC_cl["C2"]))
         .replace("Class3", abcd_c)
-        .replace("r_50_val", str(r_50))
-        .replace("N_50_val", str(int(N_50)))
+        .replace("r_50_val", str(UCC_cl["r_50"]))
+        .replace("N_50_val", str(int(UCC_cl["N_50"])))
     )
 
-    if N_50 == 0:
+    if UCC_cl["N_50"] == 0:
         txt += no_membs_50_warning
 
     txt += nasa_simbad_url.replace("XXNASAXX", cl_names[0].replace(" ", "%20")).replace(
-        "XXSIMBADXX", fname
+        "XXSIMBADXX", fname0
     )
 
     txt += posit_table_top
@@ -180,16 +191,24 @@ def main(
         txt += close_table_top
         txt += close_table + "\n"
 
-    signed_dec = "+" + str(dec) if dec >= 0 else str(dec)
+    signed_dec = (
+        "+" + str(UCC_cl["DE_ICRS_m"])
+        if UCC_cl["DE_ICRS_m"] >= 0
+        else str(UCC_cl["DE_ICRS_m"])
+    )
     txt += (
-        cds_simbad_url.replace("RADEC_CDS", "{},{}".format(ra, signed_dec))
-        .replace("RADEC_SMB", "{}%20{}".format(ra, dec))
-        .replace("XCLUSTX", fname)
+        cds_simbad_url.replace(
+            "RADEC_CDS", "{},{}".format(UCC_cl["RA_ICRS_m"], signed_dec)
+        )
+        .replace(
+            "RADEC_SMB", "{}%20{}".format(UCC_cl["RA_ICRS_m"], UCC_cl["DE_ICRS_m"])
+        )
+        .replace("XCLUSTX", fname0)
     )
 
-    txt += cl_plot.format(Qfold, fname)
+    txt += cl_plot.format(img_cont)
 
-    txt += notebook_url.format(Qfold, fname)
+    txt += notebook_url.format(Qfold, fname0)
 
     if fpars_table != "":
         txt += fpars_table_top
@@ -199,3 +218,237 @@ def main(
     contents = "".join(txt)
 
     return contents
+
+
+def positions_in_lit(DBs_json, DBs_full_data, DBs, DBs_i, row_UCC):
+    """ """
+    # Re-arrange DBs by year
+    DBs_years = [_.split("_")[0][-2:] for _ in DBs]
+    # Sort
+    sort_idxs = combine_UCC_new_DB.sort_year_digits(DBs_years)
+    # Re-arrange
+    DBs_sort = np.array(DBs)[sort_idxs]
+    DBs_i_sort = np.array(DBs_i)[sort_idxs]
+
+    table = ""
+    for i, db in enumerate(DBs_sort):
+        # Full 'db' database
+        df = DBs_full_data[db]
+
+        # Add positions
+        row_in = ""
+        for c in DBs_json[db]["pos"].split(","):
+            if c != "None":
+                # Read position as string
+                pos_v = str(df[c][int(DBs_i_sort[i])])
+                # Remove empty spaces if any
+                pos_v = pos_v.replace(" ", "")
+                if pos_v != "" and pos_v != "nan":
+                    row_in += str(round(float(pos_v), 3)) + " | "
+                else:
+                    row_in += "--" + " | "
+            else:
+                row_in += "--" + " | "
+
+        # See if the row contains any values
+        if row_in.replace("--", "").replace("|", "").strip() != "":
+            # Add reference
+            table += "|" + DBs_json[db]["ref"] + " | "
+            # Close and add row
+            table += row_in[:-1] + "\n"
+
+    # Add UCC positions
+    table += "| **UCC** |"
+    for col in ("RA_ICRS_m", "DE_ICRS_m", "Plx_m", "pmRA_m", "pmDE_m", "Rv_m"):
+        val = "--"
+        if row_UCC[col] != "" and not np.isnan(row_UCC[col]):
+            val = str(round(float(row_UCC[col]), 3))
+        table += val + " | "
+    # Close row
+    table = table[:-1] + "\n"
+
+    return table
+
+
+def carousel_div(cl_name, Qfold, fname0):
+    """Generate the plot or carousel of plots, depending on whether there is more than
+    one plot for a cluster.
+    """
+
+    # Check present plots. The order here determines the order in which plots will be
+    # shown: UCC --> HUNT23 --> CANTAT20
+    dbs_plots = []
+    for _db in ("HUNT23", "CANTAT20"):
+        plot_fpath = Path(
+            root_UCC_path + Qfold + f"/{plots_folder}/" + fname0 + f"_{_db}.webp"
+        )
+        if plot_fpath.is_file() is True:
+            dbs_plots.append(_db)
+
+    if len(dbs_plots) == 0:
+        img_cont = (
+            f"![{cl_name}](https://raw.githubusercontent.com/ucc23/{Qfold}"
+            + f"/main/plots/{fname0}.webp)"
+        )
+    else:
+        img_cont = """<div class="carousel">\n"""
+        img_cont += """<input type="radio" name="radio-btn" id="slide1" checked>\n"""
+        for _ in range(len(dbs_plots)):
+            img_cont += f"""<input type="radio" name="radio-btn" id="slide{_+2}">\n"""
+        img_cont += """<div class="slides">\n"""
+        img_cont += """<div class="slide">\n"""
+        img_cont += (
+            f"""<a href="https://raw.githubusercontent.com/ucc23/{Qfold}"""
+            + f"""/main/plots/{fname0}.webp" target="_blank">\n"""
+        )
+        img_cont += f"""<img src="https://raw.githubusercontent.com/ucc23/{Qfold}"""
+        img_cont += (
+            f"""/main/plots/{fname0}.webp" alt="{cl_name} UCC">\n</a>\n</div>\n"""
+        )
+        for _db in dbs_plots:
+            img_cont += """<div class="slide">\n"""
+            img_cont += (
+                f"""<a href="https://raw.githubusercontent.com/ucc23/{Qfold}"""
+                + f"""/main/plots/{fname0}_{_db}.webp" target="_blank">\n"""
+            )
+            img_cont += f"""<img src="https://raw.githubusercontent.com/ucc23/{Qfold}"""
+            img_cont += f"""/main/plots/{fname0}_{_db}.webp" alt="{cl_name} {_db}">\n</a>\n</div>\n"""
+        img_cont += """</div>\n<div class="indicators">\n"""
+        img_cont += """<label for="slide1">1</label>\n"""
+        for _ in range(len(dbs_plots)):
+            img_cont += f"""<label for="slide{_+2}">{_+2}</label>\n"""
+        img_cont += """</div>\n</div>"""
+
+    with open("/home/gabriel/Descargas/imag_cont.txt", "w") as text_file:
+        text_file.write(img_cont)
+
+    return img_cont
+
+
+def close_cat_cluster(df_UCC, row):
+    """ """
+    close_table = ""
+
+    if str(row["dups_fnames_m"]) == "nan":
+        return close_table
+
+    fnames0 = [_.split(";")[0] for _ in df_UCC["fnames"]]
+
+    dups_fnames = row["dups_fnames_m"].split(";")
+    dups_probs = row["dups_probs_m"].split(";")
+
+    for i, fname in enumerate(dups_fnames):
+        j = fnames0.index(fname)
+        name = df_UCC["ID"][j].split(";")[0]
+
+        vals = []
+        for col in ("RA_ICRS_m", "DE_ICRS_m", "Plx_m", "pmRA_m", "pmDE_m", "Rv_m"):
+            val = round(float(df_UCC[col][j]), 3)
+            if np.isnan(val):
+                vals.append("--")
+            else:
+                vals.append(val)
+        val = round(float(dups_probs[i]), 3)
+        if np.isnan(val):
+            vals.append("--")
+        else:
+            vals.append(val)
+
+        ra, dec, plx, pmRA, pmDE, Rv, prob = vals
+
+        close_table += f"|[{name}](/_clusters/{fname}/)| "
+        close_table += f"{int(100 * prob)} | "
+        close_table += f"{ra} | "
+        close_table += f"{dec} | "
+        close_table += f"{plx} | "
+        close_table += f"{pmRA} | "
+        close_table += f"{pmDE} | "
+        close_table += f"{Rv} |\n"
+    # Remove final new line
+    close_table = close_table[:-1]
+
+    return close_table
+
+
+def fpars_in_lit(
+    DBs_json: dict, DBs_full_data: dict, DBs: list, DBs_i: list, max_chars=7
+):
+    """
+    DBs_json: JSON that contains the data for all DBs
+    DBs_full_data: dictionary of pandas.DataFrame for all DBs
+    DBs: DBs where this cluster is present
+    DBs_i: Indexes where this cluster is present in each DB
+    """
+    # Select DBs with parameters
+    DBs_w_pars, DBs_i_w_pars = [], []
+    for i, db in enumerate(DBs):
+        # If this database contains any estimated fundamental parameters
+        if DBs_json[db]["pars"] != "":
+            DBs_w_pars.append(db)
+            DBs_i_w_pars.append(DBs_i[i])
+
+    if len(DBs_w_pars) == 0:
+        table = ""
+        return table
+    else:
+        # Re-arrange DBs by year
+        # Extract DB year
+        # This splits the DB name in a '_' and keeps the first part. It is meant to handle
+        # DBs with names such as: 'SMITH23_3'.
+        DBs_years = [_.split("_")[0][-2:] for _ in DBs_w_pars]
+        # Sort
+        sort_idxs = combine_UCC_new_DB.sort_year_digits(DBs_years)
+        # Re-arrange
+        DBs_w_pars = np.array(DBs_w_pars)[sort_idxs]
+        DBs_i_w_pars = np.array(DBs_i_w_pars)[sort_idxs]
+
+    txt = ""
+    for i, db in enumerate(DBs_w_pars):
+        # Full 'db' database
+        df = DBs_full_data[db]
+        # Add reference
+        txt_db = "| " + DBs_json[db]["ref"] + " | "
+
+        txt_pars = ""
+        # Add non-nan parameters
+        for par in DBs_json[db]["pars"].split(","):
+            # Read parameter value from DB as string
+            par_v = str(df[par][int(DBs_i_w_pars[i])])
+
+            # Trim parameter value
+            if ";" in par_v:
+                # DBs like SANTOS21 list more than 1 value per parameter
+                par_v = par_v[: int(max_chars * 2)]
+            else:
+                par_v = par_v[:max_chars]
+
+            # Remove empty spaces from param value (if any)
+            par_v = par_v.replace(" ", "")
+
+            if par_v != "" and par_v != "nan":
+                txt_pars += par + "=" + par_v + ", "
+
+        if txt_pars != "":
+            # Remove final ', '
+            txt_pars = txt_pars[:-2]
+            # Add quotes
+            txt_pars = "`" + txt_pars + "`"
+            # Combine and close row
+            txt += txt_db + txt_pars + " |\n"
+
+    table = ""
+    if txt != "":
+        # Remove final new line
+        table = txt[:-1]
+
+    return table
+
+
+def UCC_color(abcd):
+    """ """
+    abcd_c = ""
+    line = r"""<span style="color: {}; font-weight: bold;">{}</span>"""
+    cc = {"A": "green", "B": "#FFC300", "C": "red", "D": "purple"}
+    for letter in abcd:
+        abcd_c += line.format(cc[letter], letter)  # + '\n'
+    return abcd_c
