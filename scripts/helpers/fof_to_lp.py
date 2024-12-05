09bbe172d1f71080a726b2c4e2fc16912851592b
Gabriel <gabrielperren@gmail.com>
2024-12-05 10:43:30 -0300

    minor

--- scripts/helpers/fof_to_lp.py
+++ scripts/helpers/fof_to_lp.py
@@ -3,19 +3,18 @@ import json
 
 import pandas as pd
 
-df_UCC = pd.read_csv("../zenodo/UCC_cat_241130.csv")
+df_UCC = pd.read_csv("../../zenodo/UCC_cat_241130.csv")
 N = 0
 for i, fnames in enumerate(df_UCC['fnames']):
     fname0 = fnames.split(';')[0]
     if fname0.startswith('fof'):
-        # print(i)
         N += 1
 print(f"{N} FoF entries in the UCC\n")
 
 
 print("Databases with 'FoF' entries:")
 # Path to the JSON file and the directory containing the CSV files
-json_file_path = "all_dbs.json"  # Replace with the actual JSON file path
+json_file_path = "../../databases/all_dbs.json"  # Replace with the actual JSON file path
 out_path = "OUT/"
 
 # Load the JSON file
@@ -29,7 +28,7 @@ for key, value in json_data.items():
     name = value["names"]
 
     # Construct the path to the corresponding CSV file
-    csv_file_path = f"{key}.csv"
+    csv_file_path = "../../databases/" + f"{key}.csv"
 
     # Load the CSV file using pandas
     df = pd.read_csv(csv_file_path)
@@ -52,11 +51,10 @@ for key, value in json_data.items():
         are_identical = df.equals(df_new)
         if not are_identical:
             print(key, N)
-            # Save the updated DataFrame back to the CSV file
-            df_new.to_csv(
-                out_path + csv_file_path,
-                index=False,
-                na_rep="nan",
-                quoting=csv.QUOTE_NONNUMERIC,
-            )
-            # print(f"Updated and saved file: {csv_file_path}")
+            # # Save the updated DataFrame back to the CSV file
+            # df_new.to_csv(
+            #     out_path + csv_file_path,
+            #     index=False,
+            #     na_rep="nan",
+            #     quoting=csv.QUOTE_NONNUMERIC,
+            # )
