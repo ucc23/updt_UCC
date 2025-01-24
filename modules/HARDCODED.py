ed5b389b3ad0f155e82d6d6eb572a680dece0c75
Gabriel <gabrielperren@gmail.com>
2025-01-24 16:52:22 -0300

    add manual pars file path

--- modules/HARDCODED.py
+++ modules/HARDCODED.py
@@ -33,3 +33,6 @@ plots_folder = "plots"
 
 # Folder where files to be uploaded to Zenodo are stored
 zenodo_folder = "../zenodo_upload/"
+
+# Path to the file with manual OC parameters
+manual_pars_file = "manual_params.csv"
